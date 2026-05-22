aebe132fec866789c0e4490f77da4dc27b2fed10
HknAccount <harikeshwaran.palani@gmail.com>
2026-05-22 20:33:07 +0200

    add Ansible playbook to configure AWS server

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,10 +2,30 @@ provider "aws" {
   region = "eu-west-3"
 }
 
-# Creates a virtual server on AWS
+resource "aws_security_group" "satping_sg" {
+  name        = "satping-sg"
+  description = "Allow SSH"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
 resource "aws_instance" "satping" {
-  ami           = "ami-0302f42a44bf53a45"  # Amazon Linux 2 in Paris
-  instance_type = "t3.micro"               # free tier in eu-west-3
+  ami                    = "ami-0302f42a44bf53a45"
+  instance_type          = "t3.micro"
+  key_name               = "satping-key"
+  vpc_security_group_ids = [aws_security_group.satping_sg.id]
 
   tags = {
     Name = "satping-server"
